73d96102ed6d44b6eeb4eb464ed4b194152e9269
Abdullah-Alholiel <A.alholaiel@gmail.com>
2023-11-29 13:54:33 +0000

    Edits to deploy

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -1,80 +1,52 @@
 const express = require('express');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+require('dotenv').config();
 
 const app = express.Router();
 
-// Authentication and authorization middleware
+// Hashes a password before saving it to the database
+const hashPassword = async (password) => {
+    const salt = await bcrypt.genSalt(10);
+    return bcrypt.hash(password, salt);
+};
+
+// Compares a provided password with a stored hashed password
+const comparePassword = async (providedPassword, storedPassword) => {
+    return bcrypt.compare(providedPassword, storedPassword);
+};
+
+// Middleware for user authentication
 const auth = async (req, res, next) => {
     try {
-      // Retrieves the JWT token from the request headers
-      const token = req.header('Authorization').replace('Bearer ', '');
-  
-      // Verifies the JWT token using the secret key
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  
-      // Retrieves the user data from the database using the decoded JWT token
-      const user = await User.findOne({ _id: decoded._id });
-  
-      if (!user) {
-        throw new Error('User not found');
-      }
-  
-      // Sets the user data on the request object for use in other middleware functions
-      req.user = user;
-  
-      // Calls the next middleware function in the chain
-      next();
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findOne({ _id: decoded._id });
+
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        req.user = user;
+        next();
     } catch (error) {
-      // If there's an error, returns a 401 Unauthorized response
-      res.status(401).send({ error: 'Please authenticate.' });
+        res.status(401).send({ error: 'Please authenticate.' });
     }
-  };
-  
-  // Middleware function for checking authentication
-  
-  app.use(async (req, res, next) => {
+};
+
+// Middleware for user authorization
+app.use(async (req, res, next) => {
     try {
-      // Checks if the request has an Authorization header with a valid JWT token
-      const token = req.headers.authorization;
-      if (!token) {
-        return res.status(401).send({ error: 'Unauthorized' });
-      }
-  
-      // Retrieves the user data from the database using the JWT token
-      const user = await User.findOne({ token });
-  
-      if (!user) {
-        return res.status(401).send({ error: 'Unauthorized' });
-      }
-  
-      // Sets the user data on the request object for use in other middleware functions
-      req.user = user;
-  
-      // Calls the next middleware function in the chain
-      next();
+        const user = req.user;
+        if (!user.role || user.role !== 'admin') {
+            return res.status(403).send({ error: 'Forbidden' });
+        }
+        next();
     } catch (error) {
-      // If there's an error, returns a 500 Internal Server Error response
-      res.status(500).send({ error: 'Error authenticating user' });
+        res.status(500).send({ error: 'Error authorizing user' });
     }
-  });
-  
-  // Middleware function for checking authorization
-  app.use(async (req, res, next) => {
-    try {
-      // Checks if the user has the required role for the requested resource
-      const { user } = req;
-      if (!user.role || user.role !== 'admin') {
-        return res.status(403).send({ error: 'Forbidden' });
-      }
-  
-      // Calls the next middleware function in the chain
-      next();
-    } catch (error) {
-      // If there's an error, returns a 500 Internal Server Error response
-      res.status(500).send({ error: 'Error authorizing user' });
-    }
-  });
-  
-  // Exports the auth middleware function
-  module.exports = auth;
+});
+
+// Export the authentication middleware
+module.exports = { auth, hashPassword, comparePassword };
