904c761c7f60933b76b1141dc71de9fb94628bd8
Abdullah-Alholiel <A.alholaiel@gmail.com>
2023-12-14 22:36:02 +0000

    updated CORS urls

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Enable CORS for your client's origin and localhost 
 app.use(cors({
-  origin: 'https://spms-dissertation-shu.onrender.com' && 'http://localhost:3000',
+  origin: 'https://spms-dissertation-shu.onrender.com' || 'http://localhost:3000',
   credentials: true // Add this line to enable sending cookies with the request
 }));
 
